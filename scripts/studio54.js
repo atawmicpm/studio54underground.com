13e4882d3d2f8425c5de24027156bc1fb16f645e
Phillip Mispagel <atawmicpm@gmail.com>
2014-08-19 21:28:27 -0700

    cleaned up

--- scripts/studio54.js
+++ scripts/studio54.js
@@ -1,142 +1,155 @@
-// Studio54
-// code by Phillip
-
- 
 var Artists = Backbone.Collection.extend();  
  
 var artists = new Artists([
-	{ id: 1, 
-		name: 'Ethan Miller', 
-		mix: 'https://soundcloud.com/djethan/live-at-fnf-campout-xviii', 
-		photo: 'https://i1.sndcdn.com/avatars-000038731944-1lqeu7-t500x500.jpg'
-	},
-	{
-		id: 2, 
-		name: 'Chipper Guy', 
-		mix: 'https://soundcloud.com/chipper-guy/deep-end-of-the-pool', 
-		photo: 'https://i1.sndcdn.com/avatars-000023321689-cqclvs-t500x500.jpg'
-	}
+  { id: 1, 
+    name: 'Ethan Miller', 
+    mix: 'https://soundcloud.com/djethan/live-at-fnf-campout-xviii', 
+    photo: 'https://i1.sndcdn.com/avatars-000038731944-1lqeu7-t500x500.jpg'
+  },
+  {
+    id: 2, 
+    name: 'Chipper Guy', 
+    mix: 'https://soundcloud.com/chipper-guy/deep-end-of-the-pool', 
+    photo: 'https://i1.sndcdn.com/avatars-000023321689-cqclvs-t500x500.jpg'
+  }
 ]);  
 
 
-var PresalesView = Backbone.View.extend({
-
-		el: '.content',
-
-    initialize:function(){
-        this.render();
-    },
-
-    render: function() {
-
-    	var source 		= $('#presales-template').html(),
-    			template 	= Handlebars.compile(source),
-					presales 	= template(),
-					$presales = $(presales);
-
-			// debugger
-			this.$el.append(presales);
-			$presales.fadeIn();
-    }
+var app = new Backbone.Marionette.Application();
+ 
+app.addRegions({
+    appRegion: '#studio54underground'
 });
  
-// var presalesView = new PresalesView();
+app.module('App',function(module, App, Backbone, Marionette, $, _){
+     
+    module.PresalesLayoutView = Marionette.LayoutView.extend({
+        tagName: 'div',
+        id: 'presalesLayoutView',
+        className: 'content',
 
-var LineupView = Backbone.View.extend({
-    el: '.dj-superstar',
-  
-    tagName: 'li',
-
-    initialize:function(){
-        this.render();
-    },
-
-    render: function() {
-
-     var gridster = $("#lineup ul").gridster({
-            widget_margins: [10, 10],
-            widget_base_dimensions: [140, 140]
-     }).data('gridster');
-
-  		_.each(artists.models, function(artist,index){
-      	var source 		= $('#lineup-template').html(),
-      			template 	= Handlebars.compile(source),
-						artist 		= template(artist.toJSON());
-     	
-        gridster.add_widget(artist, 1,1,1,index+1);
-  		});
-    }
-
-});
+        template: '#presales-template',
  
-
-var MainView = Backbone.View.extend({
-
-  el: 'body',
-
-  events: {
-    'click #nav-lineup': 'clickLineup',
-    'click #nav-presales': 'clickPresales',
-  },
-
-  initialize: function() {
-    this.views = [];
-    this.render();
-  },
-
-  updateNav: function(event) {
-    this.$('.navbar-nav > li').removeClass('active');
-    $(event.currentTarget).addClass('active');
-  },
-
-  cleanViews: function() {
-    _(this.views).each(function(view){
-      view.remove();
+        initialize: function(){console.log('Presales: initialize');},
+        onRender: function(){console.log('Presales: onRender');},
+        onShow: function(){console.log('Presales: onShow');}
     });
-  },
+ 
+    module.LineupLayoutView = Marionette.LayoutView.extend({
+        tagName: 'div',
+        id: 'LineupLayoutView',
+        className: 'content',
+ 
+        template: '#lineup-template',
+ 
+        events: {},
+ 
+        initialize: function(){console.log('Lineup: initialize');},
+        
+        onRender: function(){
 
-  clickLineup: function(event) {
-    event.preventDefault();
-    this.updateNav(event);
-    this.cleanViews();
-    this.renderLineup();
-  },
 
-  clickPresales: function(event) {
-    event.preventDefault();
-    this.updateNav(event);
-    // this.cleanViews();
-    this.showPresales(true);
-  },
+          console.log('Lineup: onRender');
+        },
+        
+        onShow: function(){
+          var gridster = $("#lineup ul").gridster({
+            widget_margins: [10, 10],
+            widget_base_dimensions: [200, 200]
+          }).data('gridster');
 
-  renderLineup: function() {
-    this.showPresales(false);
-    var lineupView = new LineupView();
+          _.each(artists.models, function(artist,index){
+            var source    = $('#dj-template').html(),
+                template  = Handlebars.compile(source),
+                artist    = template(artist.toJSON());
 
-    // this.views.push(lineupView);
-  },
+            gridster.add_widget(artist, 1,1,1,index+1);
+          });
 
-  renderPresales: function() {
-    var presalesView = new PresalesView();
-    this.views.push(presalesView);
-  },
+          console.log('Lineup: onShow');
+        }
+    });
+ 
+     module.DetailsLayoutView = Marionette.LayoutView.extend({
+        tagName: 'div',
+        id: 'DetailsLayoutView',
+        className: 'content',
+ 
+        template: '#details-template',
+ 
+        events: {},
+ 
+        initialize: function(){console.log('Details: initialize');},
+        onRender: function(){console.log('Details: onRender');},
+        onShow: function(){console.log('Details: onShow');}
+    });
+ 
+    /* define a view; in this case a 'Layout' */
+    module.AppLayoutView = Marionette.LayoutView.extend({
+        tagName: 'div',
+        id: 'AppContainer',
+        template: '#layout-template',
+ 
+        regions: {
+            'Content' : '#content',
+        },
+ 
+        events: {
+          'click #nav-presales': 'renderPresales',
+          'click #nav-lineup': 'renderLineup',
+          'click #nav-details': 'renderDetails'
+        },
 
-  showLineup: function() {
-    // debugger;
-  },
 
-  showPresales: function(truth) {
-    this.$('.presales').toggle(truth);
-    // var presalesView = new PresalesView();
-    // this.views.push(presalesView);     
-  },
+        /* called when the view initializes, before it displays */
+        initialize: function() {
+            console.log('main layout: initialize');
+        },
+ 
+        updateNav: function(event) {
+          if (!event) return;
+          this.$('.navbar-nav > li').removeClass('active');
+          $(event.currentTarget).addClass('active');
+        },
 
-  render: function() {
-    this.showPresales(true);
-  },
+        renderPresales: function(event) {
+          var presalesLayout = new module.PresalesLayoutView();
+          this.Content.show(presalesLayout);
+          this.updateNav(event);
+        },
 
+        renderLineup: function(event) {
+          var lineupLayout = new module.LineupLayoutView();
+          this.Content.show(lineupLayout);
+          this.updateNav(event);
+        },
+
+        renderDetails: function(event) {
+          var detailsLayout = new module.DetailsLayoutView();
+          this.Content.show(detailsLayout);
+          this.updateNav(event);
+        },
+
+        onRender: function() {
+          this.renderPresales();
+        },
+ 
+        onShow: function() {
+            console.log('main layout: onShow');
+        }
+    });
+ 
+    /* Tell the module what to do when it is done loading */
+    module.addInitializer(function(){
+        /* create a new instance of the layout from the module */
+        var layout = new module.AppLayoutView();
+ 
+        /* display the layout in the region defined at the top of this file */
+        app.appRegion.show(layout);
+    });
 });
-
-var mainView = new MainView();
-
-
+ 
+/* once the DOM initializes, start the app */
+$(document).ready(function() {
+  app.start();
+});
